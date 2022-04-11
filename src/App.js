a7111ca2786eb5c12c51b94fbc8508138a4f266d
ZAELOOK <anygals2@gmail.com>
2022-04-11 20:17:47 +0900

    공부

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable*/
+
+import React, { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  let title = "개발 Blog";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>{title} </div>
+        {/* <img src={logo}></img> */}
+      </div>
+      
+      <div className="list">
+        <h3> {글제목[0]}  <span onClick={() => { 따봉변경(따봉+1) }}>🥰</span> { 따봉 } </h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+            <div className="list">
+        <h3> { 글제목[1] } </h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+            <div className="list">
+        <h3> { 글제목[2] } </h3>
+        <p>2월 19일 발행</p>
+        <hr/>
+      </div>
+
+
+
     </div>
+
+
   );
 }
 
